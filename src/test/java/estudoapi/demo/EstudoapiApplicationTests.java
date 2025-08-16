3b51312b208d5b550697340df133b3f363d640c5
Kauan Rodrigues <kaurodb@gmail.com>
2025-08-16 14:17:50 -0300

    [feat] ✨ : Estudo API (Teste no Ar)

---
+++ src/test/java/estudoapi/demo/EstudoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package estudoapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
